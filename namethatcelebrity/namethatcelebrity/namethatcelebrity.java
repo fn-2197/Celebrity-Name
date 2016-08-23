0b261d37f19e1c1821cf3f8c56393daa09c54244
fn-2197 <LoganHaas@outlook.com>
2016-08-23 13:01:47 -0600

    Add files via upload

---
+++ namethatcelebrity/namethatcelebrity/namethatcelebrity.java
@@ -0,0 +1,20 @@
+package namethatcelebrity;
+
+public class namethatcelebrity {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String s1 = "Allan Alda";
+		String s2 = "John Wayne";
+		String s3 = "Gregory Peck";
+		
+		String ss1 = s1.substring(2,8);
+		String ss2 = s2.substring(2,8);
+		String ss3 = s3.substring(2,10);
+System.out.println(ss1);
+System.out.println(ss2);
+System.out.println(ss3);
+	}
+
+}
